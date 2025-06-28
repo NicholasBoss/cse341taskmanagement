cb7ba5ee27cad67c5dcda676af7fd9d145e77362
Vilate <viknapp22@gmail.com>
2025-06-27 20:22:27 -0600

    cleaned up routes/index and implemented appointments

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,18 @@
-const routes = require('express').Router();
+const express = require('express');
+const routes = express.Router();
 
-const swagger = require('./swagger');
+const swaggerRoutes = require('./swagger');
+const appointmentRoutes = require('./appointmentRoutes');
+// const taskRoutes = require('./taskRoutes');
+// const projectRoutes = require('./projectRoutes');
 
-routes.use('/', swagger);
+routes.get('/', (req, res) => {
+    res.send('Welcome! Visit /api-docs for API documentation.');
+});
+
+routes.use('/', swaggerRoutes);
+routes.use('/appointments', appointmentRoutes);
+// routes.use('/tasks', taskRoutes);
+// routes.use('/projects', projectRoutes);
 
 module.exports = routes;
