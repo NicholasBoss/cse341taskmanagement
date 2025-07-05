cc61d673eb5ed7ff487a2ca93e7dade967abbcfa
Vilate <viknapp22@gmail.com>
2025-07-05 08:27:39 -0600

    require and call project endpoints in index

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ const swaggerRoutes = require('./swagger');
 const appointmentRoutes = require('./appointmentRoutes');
 const userRoutes = require('./userRoutes');
 const taskRoutes = require('./taskRoutes');
-// const projectRoutes = require('./projectRoutes');
+const projectRoutes = require('./projectRoutes');
 
 routes.get('/', (req, res) => {
     res.send('Welcome! Visit /api-docs for API documentation.');
@@ -15,6 +15,6 @@ routes.use('/', swaggerRoutes);
 routes.use('/appointments', appointmentRoutes);
 routes.use('/users', userRoutes);
 routes.use('/tasks', taskRoutes);
-// routes.use('/projects', projectRoutes);
+routes.use('/projects', projectRoutes);
 
 module.exports = routes;
