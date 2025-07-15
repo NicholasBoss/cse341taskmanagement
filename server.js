7ab27a47010ab346444f4eb10004a3c4e979de5c
NicholasBoss <NicholasBoss0914@gmail.com>
2025-07-15 16:09:06 -0600

    adding in my attempt at OAuth

--- server.js
+++ server.js
@@ -4,15 +4,47 @@ const routes = require('./routes');
 // const static = require("./routes/static")
 const mongodb = require('./database/connect')
 // const bodyParser = require('body-parser');
-// const passport = require('passport');
-// const session = require('express-session');
-// const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const passport = require('passport');
+const session = require('express-session');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
 require('dotenv').config();
 const app = express();
 const port = 3000;
 
 app.use(express.static('public'));
 app.use(express.json());
+
+// Session Setup
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: true,
+}));
+
+// Passport Setup
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Passport Serialization
+passport.serializeUser((user, done) => {
+  done(null, user);
+});
+
+passport.deserializeUser((user, done) => {
+  done(null, user);
+});
+
+// Passport Configuration
+passport.use(new GoogleStrategy({
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    callbackURL: process.env.GOOGLE_CALLBACK_URL || 'http://localhost:3000/auth/google/callback',
+  },
+  (accessToken, refreshToken, profile, done) => {
+    // You could save the user to a DB here
+    return done(null, profile);
+  }
+));
 
 app.use('/', routes);
 
