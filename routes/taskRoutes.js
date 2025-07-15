77c50ca0226049aca95857944b3ae20f1e21e73b
Vilate <viknapp22@gmail.com>
2025-07-15 13:19:38 -0600

    built validator code for task data and imported to routes

--- routes/taskRoutes.js
+++ routes/taskRoutes.js
@@ -9,10 +9,13 @@ const {
     deleteTask,
 } = require('../controllers/taskController');
 
+const { taskValidationRules } = require('../validators/taskValidator');
+const { validate } = require('../validators/validate');
+
 router.get('/', getAllTasks);
 router.get('/:id', getTasksById);
-router.post('/', createTask);
-router.put('/:id', updateTask);
+router.post('/', taskValidationRules(), validate, createTask);
+router.put('/:id', taskValidationRules(), validate, updateTask);
 router.delete('/:id', deleteTask);
 
 module.exports = router;
