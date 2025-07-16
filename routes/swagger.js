2868c99838baf3e1419cb8e1de802b8f353a2f7a
NicholasBoss <NicholasBoss0914@gmail.com>
2025-07-15 22:56:24 -0600

    updated swagger.json and fixed redirect URL

--- routes/swagger.js
+++ routes/swagger.js
@@ -6,7 +6,7 @@ const { GOOGLE_CLIENT_ID } = process.env;
 router.use("/api-docs", swaggerUi.serve);
 router.get("/api-docs", swaggerUi.setup(swaggerDocument, {
     swaggerOptions: {
-        oauth2RedirectUrl: "https://cse341taskmanagement.onrender.com/api-docs/oauth2-redirect.html",
+        oauth2RedirectUrl: "https://cse341taskmanagement-lif5.onrender.com/api-docs/oauth2-redirect.html",
         oauth: {
             clientId: GOOGLE_CLIENT_ID,
             scopes: "openid email profile",
